f07af485b09c80daf951d635b0e1786dfd342f52
NickStrach <strachn@wwu.edu>
2019-12-04 10:20:03 -0800

    Merging files

--- LNP.py
+++ LNP.py
@@ -63,15 +63,11 @@ def send(s, msg, code=None, cipher=None):
             utf_str += b'0'
         packed_msg = struct.pack('>i{}s'.format(len(msg)), len(msg), utf_str)
         if cipher:
-            #print("LNP.send cipher")
-            #print(packed_msg)
             repacked_msg = b''
             for byt in packed_msg:
                 repacked_msg += cipher.encrypt(bytes([byt]))
             packed_msg = repacked_msg
-            #print(repacked_msg)
         
-        #print(packed_msg)
         s.send(packed_msg)
 
 
@@ -90,13 +86,10 @@ def recv(s, msg_buffers, recv_len, msg_len, cipher_decrypter):
     try:
         msg = s.recv(2)
         if cipher_decrypter: # make sure none doesn't pass this maybe check cipher_decrypted != None
-            #print("LNP RECV CIPHER_DECRYPTER")
-            #print(msg)
             decrypted_msg = b''
             for byt in msg:
                 decrypted_msg += cipher_decrypter.decrypt(bytes([byt])) # then return cipher decrypter??
             msg = decrypted_msg
-            #print(decrypted_msg)
 
 
     # except:
@@ -125,8 +118,6 @@ def recv(s, msg_buffers, recv_len, msg_len, cipher_decrypter):
     if (s not in msg_len) and (recv_len[s] == 4):
 
         length = struct.unpack(">i", msg_buffers[s])[0]
-
-        #print(length)
 
         #Special codes are sent as negative numbers in the length field
         if length < 0:
